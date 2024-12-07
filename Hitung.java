012dc8d2ae6c60a39cd9b65ae62f2ea88d14d430
Sharone j.p wonmally <sharonejpwonmally@gmail.com>
2024-12-07 23:31:03 +0700

    Update Hitung.java

--- Hitung.java
+++ Hitung.java
@@ -6,9 +6,20 @@ public class Hitung{
     public static ArrayList<Integer> data = new ArrayList<>();
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        /* Pekerjaan anda mulai dari sini */
+        String jawab;
 
-        /* Pekerjaan anda berakhir sini */
+        do {
+            data.add(input.nextInt());
+
+            jawab = input.next().trim().toUpperCase();
+            while (!jawab.equals("Y") && !jawab.equals("T")) {
+                jawab = input.next().trim().toUpperCase();
+            }
+        } while (jawab.equals("Y"));
+
+        System.out.println("Nilai minimal : "+ nilaiMin(data));
+        System.out.println("Nilai maksimal : "+ nilaiMax(data));
+        System.out.println("Nilai rata-rata : "+ rataRata(data));
         System.out.println(Hitung.class.getDeclaredMethods().length);
         System.out.println(Hitung.class.getDeclaredFields().length);
     }
